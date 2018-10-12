ded331a02f3ea93cc258188e19e4df61a6ea6511
Unknown <opticalfalicy@gmail.com>
2018-10-12 13:06:49 -0500

    Exercises

---
+++ ex3.rb
@@ -0,0 +1,16 @@
++ "add"
+- "subtract"
+/ "divide"
+* "multiply"
+** "Exponent"
+% "Modulous"
+< "less than"
+> "greater than"
+<= "less than or equal"
+>= "greater than or equal"
+
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 / 6}"
+puts 
+
