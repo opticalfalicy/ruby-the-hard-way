2d9a0558527e6c8951cf1b786d24d2c1e89dd418
Unknown <opticalfalicy@gmail.com>
2018-10-12 12:10:38 -0500

    ex1

---
+++ ext1.rb
@@ -0,0 +1,11 @@
+puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+#puts "This is fun."
+#puts "Yay! Printing."
+#puts ""
+#puts "I'd much rather you 'not'."
+
+
+# octo is a comment
+# puts 'I "said" do not touch this.'
